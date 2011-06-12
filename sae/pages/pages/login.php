9304a0c6de8426d6a4865a802de6af14d4ff2e74
lucita287 <lucita287@gmail.com>
2011-06-12 08:23:45 +0000

    pages

--- sae/pages/pages/login.php
+++ sae/pages/pages/login.php
@@ -1,32 +1,59 @@
+<script type="text/javascript">
+$().ready(function() {
+	
+	// validate signup form on keyup and submit
+	$("#signupForm").validate({
+		rules: {
+			username: {
+				required: true,
+				minlength: 2
+			},
+			txt_password: {
+				required: true,
+				minlength: 5
+			}
+		},
+		messages: {
+			username: {
+				required: "Ingrese su usuario",
+				minlength: "Su usuario debe contener almenos 2 caracteres"
+			},
+			txt_password: {
+				required: "Ingrese su password",
+				minlength: "Su password debe contener almenos 5 caracteres"
+			}
+		}
+	});
+	
+});
+
+</script>
+
 		<div id="colOne">
 				<div id="logo">
 					<h1><a href="#">SAE-SAP</a></h1>
-					<h2> <a href="http://ingenieria-usac.edu.gt/">Facultad de Ingenieria</a></h2>
+					<h2> <a href="http://ingenieria-usac.edu.gt/">Facultad de Ingenier&iacute;a</a></h2>
 				</div>
 				<div class="box">
 				<div class="bottom">
-				<form class="jqtransform" action="post.php" method="post">	
-					<label for="user"> Acceder </label>
-							<div class="rowElem">
-									<label for="user">Usuario: </label>
-									<input name="txtUser" type="text" id="txtUser" />
-							</div>
-							<div class="rowElem">
-									<label for="password">Constrase&ntilde;a: </label> 
-									<input name="txtPassword" type="password" id="txtPassword" />
-							</div> 
-							<div class="rowElem">
-									<table>
-										<tr>
-											<td> <a href="pages/recuperar.php" class="ajaxmenu">Recuperar Contraseña</a> </td> 
-											<td> <a href="pages/registrar.php" class="ajaxmenu">Regitrarse</a> </td>
-										</tr>
+				<form  id="signupForm" action="" method="post">	
+					<label for="username"> Acceder </label>
+							<br/><label for="username">Usuario: </label> 
+							<br/><input id="username" name="username" />
+							<br/><label for="txt_password">Constrase&ntilde;a:</label>
+							<br/><input name="txt_password" type="password" id="txt_password" />
+												
+									<table> 
+									<tr>
+										<td> <a href="pages/recuperar.php" class="ajaxmenu">Recuperar Contrase&ntilde;a</a> </td> 
+										<td> <a href="pages/registrar.php" class="ajaxmenu">Regitrarse</a> </td>
+									</tr> 
 									</table>
-							</div> 
-							<div class="rowElem">
-									<input name="btnAcces" type="submit" id="btnAcces" value="Accesar" /> 
-							</div>						
+							
+							<br/><input name="btnAcces" type="submit" id="btnAcces" value="Accesar" /> 
+													
 				</form> 
 				</div>
 				</div>
 			</div>
+			
