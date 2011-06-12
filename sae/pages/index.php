a4ea74b47c8e029ff1d827ecf2d2045c89910fb8
lucita287 <lucita287@gmail.com>
2011-06-12 06:02:05 +0000

    cambio estructura de index.php

--- sae/pages/index.php
+++ sae/pages/index.php
@@ -5,52 +5,16 @@
 <?php include("includes/top_page.php"); ?>
 <body>
 	<div id="wrapper">	
-		<div id="header"> 
-			<?php include("includes/header.php"); ?>
-		</div> 
+				<?php include("includes/header.php"); ?>
 		<div id="content">
-			<div id="colOne">
-				<div id="logo">
-					<h1><a href="#">SAE-SAP</a></h1>
-					<h2> <a href="http://ingenieria-usac.edu.gt/">Facultad de Ingenieria</a></h2>
-				</div>
-				<div class="box">
-				<div class="bottom">
-				<form class="jqtransform" action="post.php" method="post">	
-					<label for="user"> Acceder </label>
-							<div class="rowElem">
-									<label for="user">Usuario: </label>
-									<input name="txtUser" type="text" id="txtUser" />
-							</div>
-							<div class="rowElem">
-									<label for="password">Constraseña: </label> 
-									<input name="txtPassword" type="password" id="txtPassword" />
-							</div> 
-							<div class="rowElem">
-									<table>
-										<tr>
-											<td> <a href="pages/recuperar.php" class="ajaxmenu">Recuperar Contraseña</a> </td> 
-											<td> <a href="pages/registrar.php" class="ajaxmenu">Regitrarse</a> </td>
-										</tr>
-									</table>
-							</div> 
-							<div class="rowElem">
-									<input name="btnAcces" type="submit" id="btnAcces" value="Accesar" /> 
-							</div>
-						
-					
-				</form> 
-				
-				</div>
-				</div>
-			</div>
+				<?php include("pages/login.php"); ?>
 			<div id="colTwo">
-				<? include("includes/pages.php"); ?>        
+				<?php include("includes/pages.php"); ?>        
 				<br style="clear:both;" />
 			</div>
 		</div>
 		<div id="footer">  
-			<?php include("includes/footer.php"); ?>
+				<?php include("includes/footer.php"); ?>
 		</div>
 	</div>
 	</body>
