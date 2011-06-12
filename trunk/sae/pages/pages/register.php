93606c87a223f7ebc8242e8306ca8889d02ea500
darn1986@gmail.com <darn1986@gmail.com@30a6b081-16c8-2ef4-444a-d01d722f37b0>
2011-06-12 21:46:35 +0000

    Nuevo Registrar

--- trunk/sae/pages/pages/register.php
+++ trunk/sae/pages/pages/register.php
@@ -3,28 +3,31 @@
 	<h2>Registrar un nuevo usuario</h2>
 	<form method="post" id="form-elements" action="">
 	<div style="padding:3px 2px;border-bottom:1px solid #ccc">Datos de acceso</div>
-		<br/><label forname="txt_user">Usuario: </label>
+		<br/><label for="txt_user">Usuario: </label>
 		<br/><input type="text" name="txt_user" id="id_username" />
-		<br/><label forname="txt_passoword">Contrase&ntilde;: </label>
+		<br/><label for="txt_passoword">Contrase&ntilde;: </label>
 		<br/><input type="password" name="txt_password" id="id_password" />
 	<div style="padding:3px 2px;border-bottom:1px solid #ccc">Datos Personales</div>	
-		<br/><label forname="txt_id">DPI / Cedula: </label><br/>
+		<br/><label for="txt_id">DPI / Cedula: </label><br/>
 		<br/><input type="text" name="txt_id" id="id_id" />
-		<br/><label forname="txt_name">Nombre: </label><br/> 
+		<br/><label for="txt_name">Nombre: </label><br/> 
 		<br/><input type="text" name="txt_name" id="id_name" />
-		<br/><label forname="txt_address">Direccion: </label><br/>
+		<br/><label for="txt_address">Direccion: </label><br/>
 		<br/><input type="text" name="txt_address" id="id_address" />
-		<br/><label forname="txt_gender">Genero: </label><br/>
+		<br/><label for="txt_gender">Genero: </label><br/>
 		<br/><input type="text" name="txt_gender" id="id_gender" />
-		<br/><label forname="txt_birthdate">Fecha de nacimiento: </label><br/>
+		<br/><label for="txt_birthdate">Fecha de nacimiento: </label><br/>
 		<br/><input type="text" name="txt_birthdate" id="id_datebrith" />
 	
 	<div style="padding:3px 2px;border-bottom:1px solid #ccc">Datos Personales</div>
-		<br/><label forname="txt_carne">Carnet: </label><br/>
+		<br/><label for="txt_carne">Carnet: </label><br/>
 		<br/><input type="text" name="txt_carne" id="id_carne" />
-		<br/><label forname="txt_datebirth">Fecha de nacimiento: </label><br/>
-		<br/><input type="text" name="txt_datebirth" id="id_datebrith" />
-		<br/><input type="submit" value=" Register " id='submit'/>
+		<br/><label for="txt_unity">Unidad: </label><br/>
+		<br/><input type="text" name="txt_unity" id="id_unity" />
+		<br/><label for="txt_extenxion">Extension: </label><br/>
+		<br/><input type="text" name="txt_extension" id="id_extension" />
+		<br/><label for="txt_career">Carrera: </label><br/>
+		<br/><input type="text" name="txt_career" id="id_career" />
 		
 	</form>
 </div>
