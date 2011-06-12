e57726fe76603619962bd65c1895d30961435f70
darn1986 <darn1986@gmail.com>
2011-06-12 05:15:39 +0000

    cambios en includes

--- sae/pages/includes/footer.php
+++ sae/pages/includes/footer.php
@@ -1,2 +1,8 @@
 
-			<p>Copyright (c) 2006 Sitename.com. All rights reserved. Design by <a href="http://freecsstemplates.org/">Free CSS Templates</a> | Courtesy <a href="http://www.openwebdesign.org" target="_blank">Open Web Design</a></p>
+			<p>OFICINAS SAE/SAP</p>
+			<p>Segundo Nivel Edificio T3 Facultad de ingenieria,</p>
+			<p>Ciudad Universitaria Zona 12,</p>
+			<p>Guatemala C.A. codigo postal 01012</p>
+			<p>Telefono (502) 2418-9113</p>
+			<p>Correo Electronico:</p>
+			<p>sae-sap@ing-usac.edu.gt</p>
