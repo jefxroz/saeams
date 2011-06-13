20256fcda1921f492c906a69dc6525d9a1279700
darn1986 <darn1986@gmail.com>
2011-06-13 00:03:23 +0000

    Formulario de Recover

--- sae/pages/pages/recover.php
+++ sae/pages/pages/recover.php
@@ -1,3 +1,18 @@
 <div name="pg_recover" style="display:none;">
 <h2>Recuperar contrase&ntilde;a </h2>
+<form method="post" id="form-elements" action="">
+	<table>
+		<tr>
+			<td><br/><div style="padding:3px 2px;border-bottom:1px solid #ccc">Datos de acceso</div></td>
+		</tr>
+		<tr>
+		<td><label for="txt_user">Usuario: </label></td>
+		<td><input type="text" name="txt_user" id="id_user" class="easyui-validatebox"  required="true"  validType="email"/></td>
+		</tr>
+		<tr style="padding:3px 2px;">
+			<td><a href="#" class="easyui-linkbutton" iconCls="icon-ok" onclick="saveUser()">Registar</a></td>  
+    		<td><a href="#" class="easyui-linkbutton" iconCls="icon-cancel" onclick="saveUser()">Cancelar</a></td>  
+		</tr>
+	</table>
+</form>
 </div>				
