79aab020bf65a7219e89c345b2160760adbb098b
lucita287@gmail.com <lucita287@gmail.com@30a6b081-16c8-2ef4-444a-d01d722f37b0>
2011-06-15 17:20:05 +0000

    Mejorar efecto de mostrar menus

--- trunk/sae/pages/menu/menu.php
+++ trunk/sae/pages/menu/menu.php
@@ -4,7 +4,7 @@
 			<h2> <a href="http://ingenieria-usac.edu.gt/">Facultad de Ingenier&iacute;a</a></h2>
 	</div>	
 	<div id="menu_acc" class="easyui-accordion" style="width:200px;">
-		<div id="menu_pn1" title='Administrar' iconCls="icon-reload" style="padding:10px;">
+		<div id="menu_pn1"  selected="true" title='Administrar' iconCls="icon-reload" style="padding:10px;">
 			<div id="menu2">
 			<ul class="menu2">
 				<li><a href="User.php" > <img src="../.././resources/images/setting1_24.png" /> Modificar Perfil</a></li>
@@ -17,7 +17,7 @@
 			</ul>
 			</div>
 		</div>
-		<div id="menu_pn2" title='Asignar' selected="true" iconCls="icon-reload" style="padding:10px;">
+		<div id="menu_pn2" title='Asignar' iconCls="icon-reload" style="padding:10px;">
 			<div id="menu2">
 			<ul class="menu2">
 				<li><a href="Course.php" > <img src="../.././resources/images/lcourse_24.png" /> Informaci&oacute;n de cursos</a></li>
@@ -28,4 +28,3 @@
 			</div>
 		</div>
 	</div>
-				
