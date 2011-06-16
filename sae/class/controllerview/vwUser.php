0f218ed62cc549baccddc0bd72148914b0003189
darn1986 <darn1986@gmail.com>
2011-06-16 03:09:40 +0000

    cambios realizados al proyecto

--- sae/class/controllerview/vwUser.php
+++ sae/class/controllerview/vwUser.php
@@ -1,20 +1,24 @@
 <?php  
-include(".././controllerclass/clrUser.php"); 
-include("././controllerclass/clrView.php"); 
-$user = intval($_REQUEST['txt_user']);
-$password = $_REQUEST['pwd_passoword'];
-$id = $_REQUEST['txt_id'];
-$name = $_REQUEST['txt_name'];
-$address = $_REQUEST['txt_address'];
-$gender = $_REQUEST['cb_gender'];
-$birthdate= $_REQUEST['dt_birthdate'];
-$carne= $_REQUEST['txt_carne'];
-$unity= $_REQUEST['txt_unity'];
-$extension= $_REQUEST['txt_extension'];
-$career=$_REQUEST['txt_career'];
-$service=$_REQUEST['lb_page'];
-echo "".$user."  ".$service;
+require_once("../controllerclass/clrUser.php"); 
 
-$objcontroler=new clrUser($user,$password,$id,$name,$address,$gender,$birthdate,$carne,$unity,$extension,$career,$service);
-$objcontroler->ejecute();
+$user = $_REQUEST['id_mail'];
+$password = $_REQUEST['id_password'];
+$id = $_REQUEST['id_id'];
+$name = $_REQUEST['id_name'];
+$surname = $_REQUEST['id_surname'];
+$address = $_REQUEST['id_address'];
+$gender = $_REQUEST['id_gender'];
+$birthdate= $_REQUEST['id_birthdate'];
+$carnet= $_REQUEST['id_carnet'];
+$unity= $_REQUEST['id_unity'];
+$extention= $_REQUEST['id_extention'];
+$career=$_REQUEST['id_career'];
+$service=$_REQUEST['id_page'];
+
+//echo " ".$user."  ".$password." ".$id." ".$name." ".$surname." ".$address." ".$gender." ".$birthdate." ".$carne." ".$unity." ".$extention." ".$career." ".$service;
+echo "Hola            ";
+$objcontroler=new clrUser($user,$password,$id,$name,$surname,$address,$gender,$birthdate,$carnet,$unity,$extention,$career,1,null,$service);
+echo $objcontroler->ejecute();
+
+
 ?>
