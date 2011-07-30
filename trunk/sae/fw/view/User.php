83bf1d814c9eea6f818dba65e3bb1fa88d80a49e
darn1986@gmail.com <darn1986@gmail.com@30a6b081-16c8-2ef4-444a-d01d722f37b0>
2011-07-30 23:26:53 +0000

    view

--- trunk/sae/fw/view/User.php
+++ trunk/sae/fw/view/User.php
@@ -134,5 +134,82 @@ else if($service==3)
 			echo json_encode(array('uno'=>$valmail->getMessage(),'dos'=>$valpassword->getMessage()));
 		}
 }
+if($service==4)
+{
+	$mail = $_REQUEST['id_mail'];
+	$valmail=new ValidatorMail('Correo',$mail,true);
 
+	$password = $_REQUEST['id_password'];
+	$passcomp = $_REQUEST['id_pass_comp'];
+	$valpassword=new ValidatorPassword('Contraseña',$password,true);
+	$valpassword->setMin(5);
+	
+	$id = $_REQUEST['id_id'];
+	$valid=new ValidatorDefault('DPI o Cedula',$id,false);
+
+	$name = $_REQUEST['id_name'];
+	$valname=new ValidatorDefault('Nombre',$name,true);
+
+	$surname = $_REQUEST['id_surname'];
+	$valsurname=new ValidatorDefault('Surname',$surname,true);
+
+	$address = $_REQUEST['id_address'];
+	$valaddress=new ValidatorDefault('Direccion',$address,true);
+
+	$gender = $_REQUEST['id_gender'];
+	$valgender=new ValidatorInteger('Genero',$gender,true);
+
+	$school = $_REQUEST['id_school'];
+	$valschool=new ValidatorInteger('Escolaridad',$school,true);
+	
+	$birthdate= $_REQUEST['id_birthdate'];
+	$valbirthdate=new ValidatorDate('Fecha',$birthdate,true);
+	
+	$phone= $_REQUEST['id_phone'];
+	$valphone=new ValidatorInteger('Telefono',$phone,false);
+	$valphone->setMin(8);
+	
+	$celular= $_REQUEST['id_celular'];
+	$valcelular=new ValidatorInteger('Celular',$phone,false);
+	$valcelular->setMin(8);
+	
+	$carnet= $_REQUEST['id_carnet'];
+	$valcarnet=new ValidatorInteger('Carnet',$carnet,false);
+	$valcarnet->setMin(4);
+	$valcarnet->setMax(10);
+	
+	$unity= $_REQUEST['id_unity'];
+	$valunity=new ValidatorInteger('Unidad',$unity,false);
+	$valunity->setMin(2);
+	
+	$extention= $_REQUEST['id_extention'];
+	$valextention=new ValidatorInteger('Extension',$extention,false);
+	$valextention->setMin(2);
+	
+	$career=$_REQUEST['id_career'];
+	$valcareer=new ValidatorInteger('Carrera',$career,false);
+	$valcareer->setMin(2);
+	
+	$captcha=$_REQUEST['id_captcha'];
+	$hidcapcha=$_REQUEST['id_hid_captcha'];
+	$valcaptcha=new ValidatorCaptcha('Captcha',$captcha,$hidcapcha,true);
+	
+	
+	if(
+		$valmail->verify() and $valpassword->verify($passcomp) and $valid->verify() and 
+	   	$valname->verify() and $valsurname->verify() and $valaddress->verify() and 
+	   	$valgender->verify() and $valschool->verify() and $valbirthdate->verify() and
+	   	$valcarnet->verify() and $valphone->verify() and $valcelular->verify() and 
+	   	$valunity->verify() and $valextention->verify() and $valcareer->verify() and $valcaptcha->verify()
+	  )
+	{
+	
+		$objcontroller=new ControlUser($valmail->getField(), $valpassword->getField(),$valid->getField(),$valname->getField(),$valsurname->getField(),$valaddress->getField(),$valgender->getField(),$valschool->getField(),$valbirthdate->getField(),$valphone->getField(),$valcelular->getField(),$valcarnet->getField(),$valunity->getField(),$valextention->getField(),$valcareer->getField(),2,null);
+		$objcontroller->registerStudent();	
+	}
+	else
+	{
+		echo json_encode(array('uno'=>$valmail->getMessage(),'dos'=>$valpassword->getMessage(),'tres'=>$valid->getMessage()));
+	}
+}
 ?>
